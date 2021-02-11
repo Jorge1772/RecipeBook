90946f04b1b83c914c8ac7ca101a2c80efe0197b
Jorge <jorgesilva1892@gmail.com>
2021-02-11 15:07:34 -0500

    adding localstorage, adding the new form fields, using use context to pass down the varaible to the child

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import RecipeList from './components/RecipeList';
+import RecipeEdit from'./components/RecipeEdit'
 import './css/index.css'
+import { v4 as uuidv4 } from 'uuid'
+
+ export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'cookingRecipes.recipes'
+
 function App() {
+const [recipes, setRecipes] = useState(sampleRecipes)
+
+const recipeContextValue={
+  handleRecipeAdd,
+  handleRecipeDelete
+}
+useEffect(()=>{
+  const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+  if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  },[])
+
+useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+},[recipes])
+
+
+
+function handleRecipeAdd(){
+  const newRecipe ={
+    id:uuidv4(),
+    name:'new',
+    servings:1,
+    cookTime:'1:00',
+    instructions:'Instr',
+    ingredients:[{id:uuidv4(),
+      name:'Name',
+      amount:'1Tbs'
+    }]
+  }
+  setRecipes([...recipes, newRecipe])
+}
+
+function handleRecipeDelete(id){
+setRecipes(recipes.filter(recipe => recipe.id !== id ))
+}
   return (
-    <>
-    <RecipeList recipes={sampleRecipes}/>
-    </>
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes}/>
+      <RecipeEdit />
+    </RecipeContext.Provider>
   );
 }
+
+
 
 const sampleRecipes =[
   {
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import Recipe from './Recipe'
-
+import React, { useContext } from 'react';
+import Recipe from './Recipe';
+import { RecipeContext } from '../App';
 
 export default function RecipeList({ recipes }) {
+   const { handleRecipeAdd } = useContext(RecipeContext)
+
     return (
         <div className='recipe-list'>
     <div>
         {recipes.map(recipes =>{
-            return (
-            <Recipe 
-            key={recipes.id} 
-            {...recipes} 
-            />
-            )
+            return ( 
+            <Recipe key={recipes.id} {...recipes}/>)
         })}
     </div>
         <div className='recipe-list_add-recipe-btn-container'> 
     </div>
-     <button className='btn btn--primary'> Add Recipe </button>
+     <button className='btn btn--primary' onClick={handleRecipeAdd} > Add Recipe </button>
      </div>
     )
 }
